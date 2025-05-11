794b3df0e022fcb04d29b413e43561c7cebf0002
sc-mitton <scm.mitton@gmail.com>
2025-05-10 18:35:22 -0700

    update profile nav list buttons

--- src/screens/profile/ProfileNavList.tsx
+++ src/screens/profile/ProfileNavList.tsx
@@ -3,7 +3,7 @@ import Foundation from "@expo/vector-icons/Foundation";
 import Entypo from "@expo/vector-icons/Entypo";
 import { useTheme } from "@shopify/restyle";
 import { SymbolView } from "expo-symbols";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableHighlight } from "react-native";
 import { Box, Text, Button } from "@components";
 import { ProfileScreenProps } from "@types";
 import { useAppSelector } from "@store/hooks";
@@ -44,10 +44,28 @@ const ProfileNavList = (props: ProfileScreenProps<"ProfileNavList">) => {
       </Box>
       <Box flexDirection="row" gap="m" marginHorizontal="s" marginTop="l">
         <Box flex={1}>
-          <Button
+          <TouchableHighlight
+            underlayColor={theme.colors.primaryText}
+            activeOpacity={0.97}
+            style={{
+              borderRadius: 15,
+            }}
             onPress={() => props.navigation.navigate("PersonalInfoModal")}
-            variant="squareCardButton"
-            icon={
+          >
+            <Box
+              backgroundColor="secondaryBackground"
+              paddingTop="ml"
+              flexDirection="column"
+              alignItems="flex-start"
+              gap="s"
+              padding="m"
+              borderRadius="l"
+              shadowColor="defaultShadow"
+              shadowOffset={{ width: 0, height: 1 }}
+              shadowOpacity={0.1}
+              shadowRadius={3}
+              elevation={5}
+            >
               <SymbolView
                 name="person.crop.circle.fill"
                 size={24}
@@ -60,16 +78,33 @@ const ProfileNavList = (props: ProfileScreenProps<"ProfileNavList">) => {
                   />
                 }
               />
-            }
-            label="Personal Info"
-          />
+              <Text>Personal Info</Text>
+            </Box>
+          </TouchableHighlight>
         </Box>
         <Box flex={1}>
-          <Button
+          <TouchableHighlight
+            underlayColor={theme.colors.primaryText}
+            activeOpacity={0.97}
+            style={{
+              borderRadius: 15,
+            }}
             onPress={() => props.navigation.navigate("AppearanceModal")}
-            variant="squareCardButton"
-            label="Appearance"
-            icon={
+          >
+            <Box
+              backgroundColor="secondaryBackground"
+              paddingTop="ml"
+              flexDirection="column"
+              alignItems="flex-start"
+              gap="s"
+              padding="m"
+              borderRadius="l"
+              shadowColor="defaultShadow"
+              shadowOffset={{ width: 0, height: 1 }}
+              shadowOpacity={0.1}
+              shadowRadius={3}
+              elevation={5}
+            >
               <SymbolView
                 name="paintbrush.pointed.fill"
                 size={24}
@@ -82,18 +117,35 @@ const ProfileNavList = (props: ProfileScreenProps<"ProfileNavList">) => {
                   />
                 }
               />
-            }
-          />
+              <Text>Appearance</Text>
+            </Box>
+          </TouchableHighlight>
         </Box>
       </Box>
-      <Box flexDirection="row" gap="m" marginHorizontal="s">
+      <Box flexDirection="row" gap="m" marginHorizontal="s" marginTop="m">
         <Box flex={1}>
-          <Button
+          <TouchableHighlight
+            underlayColor={theme.colors.primaryText}
+            activeOpacity={0.97}
+            style={{
+              borderRadius: 15,
+            }}
             onPress={() => props.navigation.navigate("EditDailyGoalModal")}
-            variant="squareCardButton"
-            flexDirection="column"
-            label="Edit Daily Goal"
-            icon={
+          >
+            <Box
+              backgroundColor="secondaryBackground"
+              paddingTop="ml"
+              flexDirection="column"
+              alignItems="flex-start"
+              gap="s"
+              padding="m"
+              borderRadius="l"
+              shadowColor="defaultShadow"
+              shadowOffset={{ width: 0, height: 1 }}
+              shadowOpacity={0.1}
+              shadowRadius={3}
+              elevation={5}
+            >
               <SymbolView
                 name="flag.fill"
                 size={24}
@@ -106,21 +158,38 @@ const ProfileNavList = (props: ProfileScreenProps<"ProfileNavList">) => {
                   />
                 }
               />
-            }
-          />
+              <Text>Edit Daily Goal</Text>
+            </Box>
+          </TouchableHighlight>
         </Box>
         <Box flex={1}>
-          <Button
+          <TouchableHighlight
+            underlayColor={theme.colors.primaryText}
+            activeOpacity={0.97}
+            style={{
+              borderRadius: 15,
+            }}
             onPress={() =>
               Alert.alert(
                 "Help",
                 "If you need help, please reach out to us at contact@northof60labs.com",
               )
             }
-            variant="squareCardButton"
-            flexDirection="column"
-            label="Help"
-            icon={
+          >
+            <Box
+              backgroundColor="secondaryBackground"
+              paddingTop="ml"
+              flexDirection="column"
+              alignItems="flex-start"
+              gap="s"
+              padding="m"
+              borderRadius="l"
+              shadowColor="defaultShadow"
+              shadowOffset={{ width: 0, height: 1 }}
+              shadowOpacity={0.1}
+              shadowRadius={3}
+              elevation={5}
+            >
               <SymbolView
                 name="questionmark.circle.fill"
                 size={24}
@@ -133,8 +202,9 @@ const ProfileNavList = (props: ProfileScreenProps<"ProfileNavList">) => {
                   />
                 }
               />
-            }
-          />
+              <Text>Help</Text>
+            </Box>
+          </TouchableHighlight>
         </Box>
       </Box>
     </Box>
