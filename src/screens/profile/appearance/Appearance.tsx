a2148ab177d6734dc2c30777cb6c82236605c3c0
sc-mitton <scm.mitton@gmail.com>
2025-05-17 12:13:08 -0700

    fix appearance modal bug

--- src/screens/profile/appearance/Appearance.tsx
+++ src/screens/profile/appearance/Appearance.tsx
@@ -13,14 +13,128 @@ import {
 } from "@store/slices/uiSlice";
 import { fontOptions } from "@constants/fonts";
 import { accentOptions } from "@constants/accents";
-import { ProfileScreenProps } from "@types";
+import { RootScreenProps } from "@types";
 
-const Appearance = (props: ProfileScreenProps<"AppearanceModal">) => {
-  const theme = useTheme();
+const Appearance = (props: RootScreenProps<"AppearanceModal">) => {
   const selectedFont = useAppSelector(selectFont);
   const selectedAccent = useAppSelector(selectAccent);
   const dispatch = useAppDispatch();
+  const theme = useTheme();
 
+  return (
+    <Box
+      backgroundColor="modalBackground"
+      padding="l"
+      paddingBottom="xxxl"
+      borderTopLeftRadius="l"
+      borderTopRightRadius="l"
+    >
+      <Box
+        borderBottomColor="seperator"
+        borderBottomWidth={1.5}
+        paddingBottom="s"
+      >
+        <Text variant="header">Font</Text>
+      </Box>
+      <Box
+        flexDirection="row"
+        flexWrap="wrap"
+        gap="xl"
+        marginTop="l"
+        justifyContent="center"
+      >
+        {fontOptions.map((font) => (
+          <Pressable key={font} onPress={() => dispatch(setFont(font))}>
+            <Box
+              borderWidth={2}
+              borderColor={font === selectedFont ? "selected" : "unselected"}
+              padding="s"
+              borderRadius="m"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Text
+                fontSize={32}
+                lineHeight={32}
+                variant={font === "inter" ? "bold" : font}
+                textTransform="capitalize"
+              >
+                Aa
+              </Text>
+            </Box>
+          </Pressable>
+        ))}
+      </Box>
+      <Box
+        borderBottomColor="seperator"
+        borderBottomWidth={1.5}
+        marginTop="xl"
+        paddingBottom="s"
+      >
+        <Text variant="header">Accent</Text>
+      </Box>
+      <Box
+        flexDirection="row"
+        flexWrap="wrap"
+        gap="m"
+        marginTop="l"
+        marginBottom="l"
+        justifyContent="center"
+      >
+        {accentOptions.map((accent) => (
+          <Pressable
+            key={accent}
+            onPress={() => {
+              if (accent === selectedAccent) {
+                dispatch(setAccent(undefined));
+              } else {
+                dispatch(setAccent(accent));
+              }
+            }}
+          >
+            <Box
+              borderRadius="full"
+              borderWidth={2}
+              borderColor={
+                accent === selectedAccent ? "selected" : "transparent"
+              }
+              padding="xs"
+            >
+              <Box
+                width={20}
+                height={20}
+                borderRadius="full"
+                alignItems="center"
+                justifyContent="center"
+                backgroundColor={accent}
+              />
+              <Box
+                borderRadius="full"
+                style={[
+                  styles.border,
+                  {
+                    borderColor: (theme.colors[accent] as string)
+                      .replace(
+                        /(\d+)%\)$/,
+                        (match) => `${Math.max(0, parseInt(match) - 30)}%)`,
+                      )
+                      .replace(
+                        /(\d+)%,/,
+                        (match) => `${Math.max(0, parseInt(match) - 20)}%,`,
+                      ),
+                  },
+                ]}
+              />
+            </Box>
+          </Pressable>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default function (props: RootScreenProps<"AppearanceModal">) {
+  const theme = useTheme();
   return (
     <BottomSheet
       onClose={() => props.navigation.goBack()}
@@ -34,122 +148,11 @@ const Appearance = (props: ProfileScreenProps<"AppearanceModal">) => {
       backdropComponent={() => <BackDrop blurIntensity={0} />}
     >
       <BottomSheetView>
-        <Box
-          backgroundColor="modalBackground"
-          padding="l"
-          paddingBottom="xxxl"
-          borderTopLeftRadius="l"
-          borderTopRightRadius="l"
-        >
-          <Box
-            borderBottomColor="seperator"
-            borderBottomWidth={1.5}
-            paddingBottom="s"
-          >
-            <Text variant="header">Font</Text>
-          </Box>
-          <Box
-            flexDirection="row"
-            flexWrap="wrap"
-            gap="xl"
-            marginTop="l"
-            justifyContent="center"
-          >
-            {fontOptions.map((font) => (
-              <Pressable key={font} onPress={() => dispatch(setFont(font))}>
-                <Box
-                  borderWidth={2}
-                  borderColor={
-                    font === selectedFont ? "selected" : "unselected"
-                  }
-                  padding="s"
-                  borderRadius="m"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <Text
-                    fontSize={32}
-                    lineHeight={32}
-                    variant={font === "inter" ? "bold" : font}
-                    textTransform="capitalize"
-                  >
-                    Aa
-                  </Text>
-                </Box>
-              </Pressable>
-            ))}
-          </Box>
-          <Box
-            borderBottomColor="seperator"
-            borderBottomWidth={1.5}
-            marginTop="xl"
-            paddingBottom="s"
-          >
-            <Text variant="header">Accent</Text>
-          </Box>
-          <Box
-            flexDirection="row"
-            flexWrap="wrap"
-            gap="m"
-            marginTop="l"
-            marginBottom="l"
-            justifyContent="center"
-          >
-            {accentOptions.map((accent) => (
-              <Pressable
-                key={accent}
-                onPress={() => {
-                  if (accent === selectedAccent) {
-                    dispatch(setAccent(undefined));
-                  } else {
-                    dispatch(setAccent(accent));
-                  }
-                }}
-              >
-                <Box
-                  borderRadius="full"
-                  borderWidth={2}
-                  borderColor={
-                    accent === selectedAccent ? "selected" : "transparent"
-                  }
-                  padding="xs"
-                >
-                  <Box
-                    width={20}
-                    height={20}
-                    borderRadius="full"
-                    alignItems="center"
-                    justifyContent="center"
-                    backgroundColor={accent}
-                  />
-                  <Box
-                    borderRadius="full"
-                    style={[
-                      styles.border,
-                      {
-                        borderColor: (theme.colors[accent] as string)
-                          .replace(
-                            /(\d+)%\)$/,
-                            (match) => `${Math.max(0, parseInt(match) - 30)}%)`,
-                          )
-                          .replace(
-                            /(\d+)%,/,
-                            (match) => `${Math.max(0, parseInt(match) - 20)}%,`,
-                          ),
-                      },
-                    ]}
-                  />
-                </Box>
-              </Pressable>
-            ))}
-          </Box>
-        </Box>
+        <Appearance {...props} />
       </BottomSheetView>
     </BottomSheet>
   );
-};
-
-export default Appearance;
+}
 
 const styles = StyleSheet.create({
   overlay: {
