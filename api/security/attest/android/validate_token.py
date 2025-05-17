02d6577bd5571207fa43df4a85b3d35ab280fc92
sc-mitton <scm.mitton@gmail.com>
2025-05-17 15:43:15 -0700

    debug backend

--- api/security/attest/android/validate_token.py
+++ api/security/attest/android/validate_token.py
@@ -27,13 +27,15 @@ def validate_challenge(redis_client: Redis, decoded_token: dict, challenge: str)
     challenge_value = challenge.split('.')[1:]
 
     request_challenge = decoded_token['requestDetails']['nonce']
+    print('request_challenge: ', request_challenge)
     request_challenge_parts = request_challenge.split('.')
     request_challenge_value = '.'.join(request_challenge_parts[0])
     request_challenge_counter = request_challenge_parts[1]
-    print('request_challenge: ', request_challenge)
 
     stored_challenge = redis_client.get(f"{CHALLENGE_PREFIX}{challenge_id}")
-    stored_challenge_counter = stored_challenge.split('.')[1]
+    stored_challenge_counter = stored_challenge[CHALLENGE_COUNTER_BIT_LENGTH:]
+    stored_challenge = stored_challenge[:CHALLENGE_COUNTER_BIT_LENGTH] + \
+        '.' + stored_challenge_counter[CHALLENGE_COUNTER_BIT_LENGTH:]
 
     if not stored_challenge == challenge_value == request_challenge_value:
         raise Exception("Invalid challenge")
